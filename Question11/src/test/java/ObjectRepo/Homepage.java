38647c40e7b2219afbc7653022f1b66174f01f68
Janardhan <janardhan@RFL348.local>
2022-11-29 18:25:05 +0530

    automation

---
+++ Question11/src/test/java/ObjectRepo/Homepage.java
@@ -0,0 +1,49 @@
+package ObjectRepo;
+
+import Utilities.Common_Functions;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Homepage {
+    Common_Functions function = new Common_Functions();
+    public Homepage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+    @FindBy(linkText = "Best Sellers")
+    private WebElement bestSellers;
+    @FindBy(id ="B07CJCGM1M")
+    private WebElement item;
+
+    @FindBy(id = "buy-now-button")
+    private WebElement buyNow;
+
+    @FindBy(id = "add-to-cart-button")
+    private WebElement addToCart;
+    @FindBy(id = "nav-cart-count-container")
+    private WebElement cart;
+    @FindBy(name = "proceedToRetailCheckout")
+    private WebElement proceedToCheckout;
+
+    public WebElement getBestSellers() {
+        return bestSellers;
+    }
+    public WebElement getItem() {
+        return item;
+    }
+    public WebElement getBuyNow() {
+        return buyNow;
+    }
+    public WebElement getAddToCart() {
+        return addToCart;
+    }
+    public WebElement getCart() {
+        return cart;
+    }
+    public WebElement getProceedToCheckout() {
+        return proceedToCheckout;
+    }
+
+}
+
---
+++ Question11/src/test/java/TestCases/Checkout_TestCase.java
@@ -0,0 +1,61 @@
+package TestCases;
+
+import ObjectRepo.Homepage;
+import Utilities.Common_Functions;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class Checkout_TestCase extends Common_Functions {
+
+    Logger logger = Logger.getLogger(Checkout_TestCase.class.getName());
+    WebDriver driver = null;
+    Homepage homepage = null;
+    @BeforeClass
+    public void launchBrowser() throws IOException {
+        driver = browserLaunch();
+        homepage = new Homepage(driver);
+        BasicConfigurator.configure();
+        //logger.info("Browser launched successfully");
+    }
+    @Test(priority = 1)
+    public void navigate() throws InterruptedException {
+        click(homepage.getBestSellers());
+        //Thread.sleep(2000);
+        click(homepage.getItem());
+        logger.info("Bestseller opened Successfully");
+    }
+    @Test(dependsOnMethods = {"navigate"})
+        public void buyNow() throws InterruptedException {
+        //Thread.sleep(2000);
+        if (homepage.getBuyNow().isDisplayed()) {
+            logger.info("BuyNow button is displayed");
+        } else {
+            logger.info("BuyNow button is not displayed");
+        }
+    }
+
+        @Test(dependsOnMethods = {"navigate"})
+            public void checkout() throws InterruptedException {
+        click(homepage.getAddToCart());
+        //Thread.sleep(2000);
+        click(homepage.getCart());
+        //Thread.sleep(2000);
+        if(homepage.getProceedToCheckout().isDisplayed()) {
+            logger.info("proceed-to-checkout button is displayed");
+        } else {
+            logger.info("proceed-to-checkout button is not displayed");
+        }
+    }
+
+    @AfterClass
+    public void teardown() {
+        driver.quit();
+        logger.info("successfully closed the browser");
+    }
+}
